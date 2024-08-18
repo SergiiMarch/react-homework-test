1f6ffd72b46c6ad28d44c803fcf155b237f5b862
Sergii Marchenko <marikmarchelo@gmail.com>
2024-08-18 20:05:34 +0300

    Add commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
-import data from './data.json';
-import data2 from './data2.json';
-import ProductList from './components/ProductList/ProductsList';
-import ProductList2 from './components/ProductList2/ProductsList2';
+// import data from './data.json';
+// import data2 from './data2.json';
+// import ProductList from './components/ProductList/ProductsList';
+// import ProductList2 from './components/ProductList2/ProductsList2';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
 
+console.log(Sidebar);
 const App = () => {
+  const [clicks, setClicks] = useState(() => {
+    const savedClicks = window.localStorage.getItem('saved-clicks');
+
+    return savedClicks !== null ? Number(savedClicks) : 0;
+  });
+
+  const [isSideBaarOpen, setIsSideBaarOpen] = useState(false);
+  const openSidebar = () => setIsSideBaarOpen(true);
+  const closeSidebar = () => setIsSideBaarOpen(false);
+
+  const [date, setDate] = useState(new Date());
+
+  const color = clicks % 5 === 0 ? 'green' : 'grey';
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-clicks', clicks);
+  }, [clicks]);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = color;
+  }, [color]);
+
+  useEffect(() => {
+    console.log('New date', date);
+  }, [date]);
+
   return (
-    <>
-      <ProductList products={data} />
-      <ProductList2 products={data2} />
-    </>
+    <div>
+      <h1>Effects in React</h1>
+      <button onClick={() => setClicks(clicks + 1)}>
+        You clicked {clicks} times
+      </button>
+      <button onClick={() => setClicks(0)}>Reset</button>
+      <button onClick={() => setDate(new Date())}>Update Date</button>
+      <button onClick={openSidebar}>Open modal</button>
+      {isSideBaarOpen && <Sidebar onClose={closeSidebar} />}
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,44 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// import { App } from 'components/App';
-// import './index.css';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-//////////////////////////////////////////////////////////////////////////////
-// const title = <h2 className="title-js">ALBERTO MEHICANO</h2>;
-// const textP = <p id="qwe333">Made in Armenia</p>;
-// const div = (
-//   <div id="1221" atrr="arter" className="div -js">
-//     {title}
-//     {textP}
-//   </div>
-// );
-
-// console.log(div);
-
-// ReactDOM.createRoot(root).render(div);
-
-////////////////////////////////////////////////////////////////////////////
-
-const root = document.getElementById('root');
-
-ReactDOM.createRoot(root).render(<App />);
-
-var number = function (busStops) {
-  // Good Luck!
-};
-console.log(
-  number(
-    [
-      [10, 0],
-      [3, 5],
-      [5, 8],
-    ],
-    5
-  )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
