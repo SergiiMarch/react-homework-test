5082b55f62839c628f753c13c624e8be4a5914b1
Sergii Marchenko <marikmarchelo@gmail.com>
2024-08-27 21:56:09 +0300

    add commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,92 @@
-// import data from './data.json';
-// import data2 from './data2.json';
-// import ProductList from './components/ProductList/ProductsList';
-// import ProductList2 from './components/ProductList2/ProductsList2';
+// import { useEffect } from 'react';
+// import axios from 'axios';
+import axios from 'axios';
+import { Field, Formik, Form } from 'formik';
 import { useState } from 'react';
-import { useEffect } from 'react';
-import Sidebar from './components/Sidebar/Sidebar';
+// import ArticleList from './ArticleList/ArticleList';
+// import ClipLoader from 'react-spinners/ClipLoader';
 
-const savedClickLocal = () => {
-  const savedClicks = window.localStorage.getItem('saved-clicks');
-  return savedClicks !== null ? Number(savedClicks) : 0;
-};
+export default function App() {
+  const [user, setUser] = useState(null);
 
-const savedModalState = () => {
-  const savedState = window.localStorage.getItem('saved-modal');
-  return savedState === 'true'; // Перетворюємо рядок у boolean
-};
+  const handleSearch = async values => {
+    console.log(values);
 
-const App = () => {
-  const [clicks, setClicks] = useState(savedClickLocal);
-
-  const [isSideBaarOpen, setIsSideBaarOpen] = useState(savedModalState);
-  const openSidebar = () => setIsSideBaarOpen(true);
-  const closeSidebar = () => setIsSideBaarOpen(false);
-
-  const [date, setDate] = useState(new Date());
-
-  const color = clicks % 5 === 0 ? 'green' : 'grey';
-
-  useEffect(() => {
-    window.localStorage.setItem('saved-modal', isSideBaarOpen);
-  }, [isSideBaarOpen]);
-
-  useEffect(() => {
-    window.localStorage.setItem('saved-clicks', clicks);
-  }, [clicks]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = color;
-  }, [color]);
-
-  useEffect(() => {
-    console.log('New date', date);
-  }, [date]);
+    const response = await axios.get(
+      `http://api.github.com/users/${values.username}`
+    );
+    setUser(response.data);
+  };
 
   return (
     <div>
-      <h1>Effects in React</h1>
-      <button onClick={() => setClicks(clicks + 1)}>
-        You clicked {clicks} times
-      </button>
-      <button onClick={() => setClicks(0)}>Reset</button>
-      <button onClick={() => setDate(new Date())}>Update Date</button>
-      <button onClick={openSidebar}>Open modal</button>
-      {isSideBaarOpen && <Sidebar onClose={closeSidebar} />}
+      <h1>HTTP requests in React</h1>
+      <Formik initialValues={{ username: '' }} onSubmit={handleSearch}>
+        <Form>
+          <Field type="text" name="username" />
+          <button type="submit">Search</button>
+        </Form>
+      </Formik>
+
+      {user && (
+        <div>
+          <img src={user.avatar_url} alt={user.name} width="180" />
+          <p>
+            <a href={user.html_url}>{user.name}</a>
+          </p>
+          <p>Followers: {user.followers}</p>
+          <p>{user.bio}</p>
+        </div>
+      )}
     </div>
   );
-};
+}
 
-export default App;
+// export default function App() {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(false);
+
+//   const override = {
+//     display: 'block',
+//     margin: '0 auto',
+//     borderColor: 'red',
+//   };
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       setLoading(true);
+
+//       try {
+//         const response = await axios.get(
+//           'https://hn.algolia.com/api/v1/search?query=react'
+//         );
+//         setArticles(response.data.hits);
+//       } catch (error) {
+//         console.log(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Latest articles</h1>
+
+//       {loading ? (
+//         <ClipLoader
+//           color="red"
+//           loading={loading}
+//           cssOverride={override}
+//           size={150}
+//           aria-label="Loading Spinner"
+//           data-testid="loader"
+//         />
+//       ) : (
+//         <ArticleList items={articles} />
+//       )}
+//     </div>
+//   );
+// }
